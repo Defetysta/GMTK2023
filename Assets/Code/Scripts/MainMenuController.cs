1f14b5460a399f44488903e8e9e1652842e2374d
Tadeusz Nowak <tadeusznowak95@gmail.com>
2023-07-09 11:40:04 +0200

    Add settings view with resolution; Add credits

--- Assets/Code/Scripts/MainMenuController.cs
+++ Assets/Code/Scripts/MainMenuController.cs
@@ -11,22 +11,29 @@ public class MainMenuController : MonoBehaviour
 	[SerializeField]
 	private Button settingsButton;
 	[SerializeField]
+	private Button creditsView;
+	[SerializeField]
 	private Button quitGameButton;
 	
 	[SerializeField]
 	private SettingsView settingsView;
+	[SerializeField]
+	private GameObject creditsViewObject;
 
 	private void Awake()
 	{
 		startGameButton.onClick.AddListener(StarGame);
 		quitGameButton.onClick.AddListener(QuitGame);
 		settingsButton.onClick.AddListener(OpenSettings);
+		creditsView.onClick.AddListener( () => creditsViewObject.SetActive(true));
 	}
 
 	private void OnDestroy()
 	{
 		startGameButton.onClick.RemoveAllListeners();
 		quitGameButton.onClick.RemoveAllListeners();
+		settingsButton.onClick.RemoveAllListeners();
+		creditsView.onClick.RemoveAllListeners();
 	}
 
 	private void StarGame()
--- Assets/Code/Scripts/SettingsView.cs
+++ Assets/Code/Scripts/SettingsView.cs
@@ -1,6 +1,41 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsView : MonoBehaviour
 {
-    
+	[SerializeField]
+	private TMP_Dropdown resolutionsDropdown;
+
+	[SerializeField]
+	private Toggle fullscreenToggle;
+	
+	
+	private List<(int, int)> resolutions = new() { (1024, 768), (1280, 720), (1366, 768), (1920, 1080), (2560, 1440) };
+
+	private void Awake()
+	{
+		resolutionsDropdown.onValueChanged.AddListener(SetScreenSize);
+		fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
+	}
+
+	private void OnDestroy()
+	{
+		resolutionsDropdown.onValueChanged.RemoveAllListeners();
+		fullscreenToggle.onValueChanged.RemoveAllListeners();
+	}
+
+	private void SetScreenSize(int index)
+	{
+		bool isFullscreen = Screen.fullScreen;
+
+		var desiredResolution = resolutions[index];
+		Screen.SetResolution(desiredResolution.Item1, desiredResolution.Item2, isFullscreen);
+	}
+
+	private void SetFullscreen(bool desiredValue)
+	{
+		Screen.fullScreen = desiredValue;
+	}
 }
