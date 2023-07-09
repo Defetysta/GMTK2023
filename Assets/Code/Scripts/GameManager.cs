b3d9380c2b0e6aa2b3f004bb9fc57712742a7507
Tadeusz Nowak <tadeusznowak95@gmail.com>
2023-07-09 12:43:23 +0200

    Cache enemy moveset at the start of the turn and sum it to display intents

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,11 +30,20 @@ public class GameManager : MonoBehaviour
 	[SerializeField]
 	private Deck playerDeck;
 
+	[SerializeField]
+	private TextMeshProUGUI attackIntent;
+	[SerializeField]
+	private TextMeshProUGUI defendIntent;
+	[SerializeField]
+	private TextMeshProUGUI healIntent;
+
 	public Button startTurnButton;
 
 	private int enemyMoveCounter = 0;
 	private Enemy enemyCombatControllerCopy;
 	private FighterStats playerCombatControllerCopy;
+
+	private List<CardStatsBase> enemyCardsThisTurn = new();
 	private void Awake()
 	{
 		enemyCombatControllerCopy = Instantiate(enemyCombatController);
@@ -52,6 +63,16 @@ public class GameManager : MonoBehaviour
 
 	private IEnumerator PlayerTurn()
 	{
+		var enemyMoves = enemyCombatControllerCopy.Moveset.Moves;
+
+		if (enemyMoveCounter >= enemyMoves.Length)
+		{
+			enemyMoveCounter = 0;
+		}
+		enemyCardsThisTurn.Clear();
+
+		DisplayEnemyIntents(enemyMoves);
+
 		int i = 0;
 
 		playerHand.AddCards(playerDeck.DrawCards());
@@ -80,20 +101,48 @@ public class GameManager : MonoBehaviour
 		playerStats.Display(playerCombatControllerCopy);
 	}
 
-	private void HandleEnemyMoves()
+	private void DisplayEnemyIntents(Moveset.MovesGroup[] enemyMoves)
 	{
-		var enemyMoves = enemyCombatControllerCopy.Moveset.Moves;
+		int totalEnemyDamage = 0;
+		int totalEnemyArmor = 0;
+		int totalEnemyHealing = 0;
 
-		if (enemyMoveCounter >= enemyMoves.Length)
+		var actions = enemyMoves[enemyMoveCounter].Actions;
+		for (int i = 0; i < actions.Length; i++)
 		{
-			enemyMoveCounter = 0;
+			enemyCardsThisTurn.Add(actions[i].GetStats());
+
+			// var cards = enemyMoves[enemyMoveCounter].Actions;
+			// for (int j = 0; j < cards.Length; j++)
+			{
+				// var cardStats =  cards[j].GetStats();
+				var cardStats =  actions[i].GetStats();
+				if (cardStats is CardSimpleAttack attackCard)
+				{
+					totalEnemyDamage += attackCard.EffectValue;
+				}
+				else if (cardStats is CardSimpleDefend defenseCard)
+				{
+					totalEnemyArmor += defenseCard.EffectValue;
+				}
+				else if (cardStats is CardSimpleHeal healCard)
+				{
+					totalEnemyHealing += healCard.EffectValue;
+				}
+			}
 		}
 
-		for (int j = 0; j < enemyMoves[enemyMoveCounter].Actions.Length; j++)
+		attackIntent.text = totalEnemyDamage.ToString();
+		defendIntent.text = totalEnemyArmor.ToString();
+		healIntent.text = totalEnemyHealing.ToString();
+	}
+
+	private void HandleEnemyMoves()
+	{
+		for (int j = 0; j < enemyCardsThisTurn.Count; j++)
 		{
-			var stats = enemyMoves[enemyMoveCounter].Actions[j].GetStats();
-			var target = stats.TargetEnemy ? playerCombatControllerCopy : enemyCombatControllerCopy.Stats;
-			stats.ApplyEffect(target);
+			var target = enemyCardsThisTurn[j].TargetEnemy ? playerCombatControllerCopy : enemyCombatControllerCopy.Stats;
+			enemyCardsThisTurn[j].ApplyEffect(target);
 		}
 
 		enemyMoveCounter++;
